a5f2d74f67daf093d460e080e6022c77fdef7938
Oskar Barcz <kontakt@archi-tektur.pl>
2018-10-17 17:42:41 +0200

    Created custom exception to ArchFW, it saves all data in logs.

---
+++ src/ArchFW/Exceptions/ArchFWException.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * ArchFramework (ArchFW in short) is universal template for server-side rendered applications and services.
+ * ArchFW comes with pre-installed router and JSON API functionality.
+ * Visit https://github.com/archi-tektur/ArchFW/ for more info.
+ *
+ * PHP version 7.2
+ *
+ * @category  Framework / Template
+ * @package   ArchFW
+ * @author    Oskar Barcz <kontakt@archi-tektur.pl>
+ * @copyright 2018 Oskar 'archi_tektur' Barcz
+ * @license   MIT
+ * @version   2.5.0
+ * @link      https://github.com/archi-tektur/ArchFW/
+ */
+
+namespace ArchFW\Exceptions;
+
+use ArchFW\Controller\Logger;
+use Exception;
+use Throwable;
+
+/**
+ * Special ArchFW Exception that logs every thrown exception to logs. Allows to monitor when and why something has
+ * thrown an exception.
+ *
+ * @package ArchFW\Exceptions
+ */
+class ArchFWException extends Exception
+{
+    /**
+     * ArchFWException constructor enforces giving an message, it also loads all data to special log file.
+     *
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct(string $message, int $code = 0, Throwable $previous = null)
+    {
+        $Log = new Logger(CONFIG['app']['exceptionLogPath']);
+        $Log->log($message, $code);
+        parent::__construct($message, $code, $previous);
+    }
+}
