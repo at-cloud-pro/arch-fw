286acc3787d4feeb8409b49cb383ae771c4ea16d
Oskar Barcz <kontakt@archi-tektur.pl>
2018-11-17 17:21:19 +0100

    Created custom exception run when Router class did not found any paths

---
+++ src/ArchFW/Exceptions/RouteNotFoundException.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * ArchFramework (ArchFW in short) is universal template for server-side rendered applications and services.
+ * ArchFW comes with pre-installed router and JSON API functionality.
+ * Visit https://github.com/archi-tektur/ArchFW/ for more info.
+ *
+ * PHP version 7.2
+ *
+ * @category  Framework/Boilerplate
+ * @package   ArchFW
+ * @author    Oskar Barcz <kontakt@archi-tektur.pl>
+ * @copyright 2018 Oskar 'archi_tektur' Barcz
+ * @license   MIT
+ * @version   4.0.0
+ * @link      https://github.com/archi-tektur/ArchFW/
+ */
+
+namespace ArchFW\Exceptions;
+
+use Exception;
+use Throwable;
+
+/**
+ * Thrown when route do not exist
+ *
+ * @package ArchFW\Exceptions
+ */
+class RouteNotFoundException extends Exception implements Throwable
+{
+
+}
