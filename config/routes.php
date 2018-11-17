bd853ae05e10bc6bda8cc302b2fad6fd10540ff2
Oskar Barcz <kontakt@archi-tektur.pl>
2018-11-17 17:12:36 +0100

    Added entry to routes.php

--- config/routes.php
+++ config/routes.php
@@ -18,14 +18,20 @@
 return [
     # Add adresses to our router. Key here is a URL adress user enters, and value is name of wrapper and twig files.
     # When file is in subdirectory, you can use '/', e.g. 'login/recoverpassword'.
-    'APProuter' => [
+    'APProuter'         => [
         '/' => 'index',
     ],
 
     # Router in API is matching URL (key here) and wrapper file name (value here)
-    'APIrouter' => [
+    'APIrouter'         => [
         '/test'        => 'test',
         '/routercheck' => 'routercheck',
         '/auth'        => 'auth',
     ],
+
+    # Redirect all routes that does not match the above scheme to other, defined above route
+    # set FALSE to turn off this function
+    # set STRING with route to turn on this function
+    'redirectOnNoMatch' => false,
+
 ];
