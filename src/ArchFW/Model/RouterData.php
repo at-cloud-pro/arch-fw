d2720692f6863145d1fda1990ff313ec0bed0eb8
Oskar Barcz <kontakt@archi-tektur.pl>
2018-11-19 19:04:33 +0100

    Commenting code

--- src/ArchFW/Model/RouterData.php
+++ src/ArchFW/Model/RouterData.php
@@ -8,24 +8,46 @@
 
 namespace ArchFW\Model;
 
-/*
+/**
  * Property design pattern object
+ *
+ * @package ArchFW\Model
  */
-
 class RouterData
 {
+    /**
+     * @var array datas
+     */
     private static $data = [];
 
+    /**
+     * Sets value
+     *
+     * @param string $key key where data can be found
+     * @param mixed $value value to store
+     */
     public static function set(string $key, $value): void
     {
         self::$data[$key] = $value;
     }
 
+    /**
+     * Returns value
+     *
+     * @param string $key get value with given key
+     * @return mixed datas
+     */
     public static function get(string $key)
     {
         return self::$data[$key];
     }
 
+    /**
+     * Check if selected key stores any data
+     *
+     * @param string $key key where data may be found
+     * @return bool true if does, false if not
+     */
     public static function exists(string $key): bool
     {
         return isset(self::$data[$key]);
