a1aa702f812bef2f14b806d5412f35d6cf82208e
Oskar Barcz <kontakt@archi-tektur.pl>
2018-11-27 18:37:08 +0100

    Introduced class to safe link encoding with BASE64

---
+++ src/ArchFW/Controllers/Utils/UriEncoder.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * ArchFramework (ArchFW in short) is universal template for server-side rendered applications and services.
+ * ArchFW comes with pre-installed router and JSON API functionality.
+ * Visit https://github.com/archi-tektur/ArchFW/ for more info.
+ *
+ * PHP version 7.2
+ *
+ * @category  Framework/Boilerplate
+ * @package   ArchFW
+ * @author    Oskar Barcz <kontakt@archi-tektur.pl>
+ * @copyright 2018 Oskar 'archi_tektur' Barcz
+ * @license   MIT
+ * @version   2.6.0
+ * @link      https://github.com/archi-tektur/ArchFW/
+ */
+
+namespace ArchFW\Controllers\Utils;
+
+use function base64_decode;
+use function base64_encode;
+use function strtr;
+
+/**
+ * Utilities used to encode and decode values in BASE64 URL-save style
+ *
+ * @package ArchFW\Controller\Utils
+ */
+class UriEncoder
+{
+
+    private const DEFAULT_KEY_REPLACEMENT = '._-';
+
+    /**
+     * Decodes string in BASE64 that can be safely used in links
+     *
+     * @param string $input
+     * @return string
+     */
+    public static function encode(string $input)
+    {
+        return strtr(base64_encode($input), '+/=', self::DEFAULT_KEY_REPLACEMENT);
+    }
+
+    /**
+     * Decodes given value from URL-safe BASE64
+     *
+     * @param $input
+     * @return bool|string
+     */
+    public static function decode($input)
+    {
+        return base64_decode(strtr($input, self::DEFAULT_KEY_REPLACEMENT, '+/='));
+    }
+
+}
