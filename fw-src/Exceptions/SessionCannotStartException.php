1254c7c29d65fe7d7243415e7cdce98ec7c0f7c6
Oskar 'archi_tektur' Barcz <kontakt@archi-tektur.pl>
2019-06-18 23:16:17 +0200

    Added session storage implementation

---
+++ fw-src/Exceptions/SessionCannotStartException.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace ArchFW\Exceptions;
+
+use RuntimeException;
+
+class SessionCannotStartException extends RuntimeException
+{
+}
--- fw-src/Storage/SessionStorage.php
+++ fw-src/Storage/SessionStorage.php
@@ -2,7 +2,55 @@
 
 namespace ArchFW\Storage;
 
+use ArchFW\Exceptions\SessionCannotStartException;
+
+/**
+ * Stores data in session
+ *
+ * @package ArchFW\Storage
+ */
 class SessionStorage implements StorageInterface
 {
+    /**
+     * Initiates new session storage
+     */
+    public function construct(): void
+    {
+        if (isset($_SESSION['sessionInitKey']) && !session_start(['sessionInitKey' => true])) {
+            throw new SessionCannotStartException('Session cannot start.');
+        }
+    }
 
+    /**
+     * Retrieve value from storage
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function get(string $key)
+    {
+        return $_SESSION[$key];
+    }
+
+    /**
+     * Set value in storage
+     *
+     * @param string $key
+     * @param        $value
+     */
+    public function set(string $key, $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    /**
+     * Checks if value exists in storage
+     *
+     * @param $key
+     * @return bool
+     */
+    public function exist($key): bool
+    {
+        return array_key_exists($key, $_SESSION);
+    }
 }
