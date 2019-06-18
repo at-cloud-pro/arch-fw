b42a0d2dfdbd6a14df49294dc80d0680dbfc69c7
Oskar 'archi_tektur' Barcz <kontakt@archi-tektur.pl>
2019-06-18 23:22:41 +0200

    Added local storage implementation

--- fw-src/Storage/LocalStorage.php
+++ fw-src/Storage/LocalStorage.php
@@ -2,7 +2,48 @@
 
 namespace ArchFW\Storage;
 
-class LocalStorage
+class LocalStorage implements StorageInterface
 {
+    /**
+     * There are stored all values
+     *
+     * @var array
+     */
+    private $storage = [];
 
+    /**
+     * Retrieve value from storage
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function get(string $key)
+    {
+        if ($this->exist($key)) {
+            return $this->storage[$key];
+        }
+        return null;
+    }
+
+    /**
+     * Set value in storage
+     *
+     * @param string $key
+     * @param        $value
+     */
+    public function set(string $key, $value): void
+    {
+        $this->storage[$key] = $value;
+    }
+
+    /**
+     * Checks if value exists in storage
+     *
+     * @param $key
+     * @return bool
+     */
+    public function exist($key): bool
+    {
+        return array_key_exists($key, $this->storage);
+    }
 }
